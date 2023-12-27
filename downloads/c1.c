6fae8451ae43bdc91bea85318b7b7b330f82ab80
41223249 <144989816+41223249@users.noreply.github.com>
2023-12-27 16:24:54 +0000

    新增c_EX頁面

---
+++ downloads/c1.c
@@ -0,0 +1,9 @@
+/* ====================
+ Say Hello World!.
+==================== */
+#include <stdio.h>
+void main()
+{
+ /* 印出 Hello */
+printf("Hello World!");
+}
---
+++ downloads/c2.c
@@ -0,0 +1,12 @@
+/* ====================
+ Say Hello World! Bye Bye.
+==================== */
+#include <stdio.h>
+int main()
+{
+/* 印出 Hello World! Bye Bye */
+printf("Hello World! ");
+printf("Bye ");
+printf("Bye");
+return 0;
+}
